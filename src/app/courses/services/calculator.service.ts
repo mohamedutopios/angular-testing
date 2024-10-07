441926439dd86003bd1869877166f691b693c819
mohamed <mohamed@utopios.net>
2024-10-07 17:47:38 +0200

    add change

--- src/app/courses/services/calculator.service.ts
+++ src/app/courses/services/calculator.service.ts
@@ -7,21 +7,19 @@ import {LoggerService} from './logger.service';
 })
 export class CalculatorService {
 
-
   constructor(private logger: LoggerService) {
 
   }
 
-  add(n1: number, n2:number) {
+     add(n1:number, n2:number) {
     this.logger.log("Addition operation called");
-    return n1 + n2;
+    return null;
   }
 
-  subtract(n1: number, n2:number) {
+  subtract(n1:number, n2:number) {
     this.logger.log("Subtraction operation called");
-    return n1 - n2;
+    return null;
   }
-
 
 }
 
